0deca1a667f101c16a076c07d718ee07bebc53ae
Abhiram <abhiramvsa7@gmail.com>
2025-09-12 14:15:05 +0530

    Implement VisuAgent class with context loading and initialization of voice interaction components

---
+++ agent/visu.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from livekit.agents import function_tool
+from livekit.agents import jupyter
+from livekit import agents
+from livekit.agents import Agent, AgentSession, JobContext, WorkerOptions
+from livekit.plugins import openai, silero, cartesia, deepgram
+import sys
+
+current_dir = Path(__file__).resolve().parent 
+parent_dir = current_dir.parent 
+sys.path.insert(0, str(parent_dir))
+
+from context.context import load_context
+from config.settings import Settings
+
+
+
+class VisuAgent(Agent):
+    """VISUUUU"""
+
+    def __init__(self):
+        
+        settings = Settings()
+        
+        repo_root = Path(__file__).resolve().parents[1]
+        prompt_path = repo_root / "prompts" / "prompt.txt"
+        rules_path = repo_root / "prompts" / "rules.txt"
+        context = load_context()
+        
+        
+        prompt_text = prompt_path.read_text(encoding="utf-8").strip()
+        rules_text = rules_path.read_text(encoding="utf-8").strip()
+        
+
+        llm = openai.LLM.with_cerebras(model="llama-3.3-70b", api_key= settings.CEREBRAS_API_KEY )
+        stt = deepgram.STT(api_key=settings.DEEPGRAM_API_KEY)
+        tts = cartesia.TTS(voice="bf0a246a-8642-498a-9950-80c35e9276b5", api_key= settings.CARTESIA_API_KEY)
+        vad = silero.VAD.load()
+
+        instructions = f"""
+        {prompt_text}
+
+        {rules_text}
+
+        Additional reference context:
+        {context}
+        """
+
+        super().__init__(
+            instructions=instructions,
+            stt=stt, llm=llm, tts=tts, vad=vad
+        )
+
+    async def on_enter(self):
+        """Called when entering this agent"""
+        print("Current Agent: VISUUU")
+        await self.session.say("Hi, I'm VISU. I would love to chat")
+
+
+print("✅ VISU ready")
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from visu-reborn!")
+from livekit.agents import Agent, AgentSession, JobContext, WorkerOptions, jupyter
+from livekit import agents
 
+from agent.visu import VisuAgent
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def agent_entrypoint(ctx: JobContext):
+    """Simple multi-agent entry point"""
+    await ctx.connect()
+
+    # Create session
+    session = AgentSession()
+
+    # Start with sales agent
+    await session.start(
+        agent=VisuAgent(),
+        room=ctx.room
+    )
 
 if __name__ == "__main__":
-    main()
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=agent_entrypoint))
